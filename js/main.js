1aa3d61f2f65dd53ed9540fb72d6301ae85b5983
Dgibbins <nayr_ryan@hotmail.com>
2017-02-15 16:09:25 -0500

    Clap On Clap Off

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 
 
+$(document).ready(function(){
+    $('body').addClass("light");
+
+  $('.switch button').on('click', function(){
+    $(this).toggleClass("on off");
+    $('body').toggleClass("light dark");
+
+    if ($('body').attr("class") === 'light'){
+      $('.status').html("Lights on!");
+    } else {
+      $('.status').html("Lights off");
+    }
+  });
+});
